15eb00af2f7ab3ad06d161a31918152e4ac284eb
Mattia <mattiapizzolitto141@gmail.com>
2021-02-09 19:08:57 +0100

    Updated .gitignore, reworked python script

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv)
 {
-    std::vector<std::pair<int, double>> v{{1, 1.00}, {5, 9.99}, {10, 10.00}};
+    std::vector<std::pair<int, double>> v{{1, 0.53}, {5, 9.99}, {10, 14.002}, {100, 140.002}, {1000, 1400.002}};
     std::vector<double> v1{};
 
     auto s = generate_random_str(5000, 10);
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
         std::cout << i << "ns\n";
     }
 
-    write_csv("test.csv", v, "N", "TIME");
+    write_csv("tempi.csv", v, "N", "TIME");
 
     return EXIT_SUCCESS;
 }
--- src/plotResults.py
+++ src/plotResults.py
@@ -1,21 +1,28 @@
 import csv
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # apro il file csv e creo una lista con il nome delle colonne e una con i risultati
-with open("/content/tempi.csv", "r", encoding='utf-8-sig') as f:
-    lettore = csv.reader(f, delimiter=";")
+with open("tempi.csv", "r", encoding='utf-8-sig') as f:
+    lettore = csv.reader(f, delimiter=",")
     col_names = next(lettore)
     results = []
     for line in lettore:
         results.append(line)
+        print(line)
 
 # creo un dataframe
 data = pd.DataFrame()
 
 data[col_names[0]] = [item[0] for item in results]
-
 data[col_names[1]] = [item[1] for item in results]
 
-sns.scatterplot(data.diversi_n, data.tempi)
+print(data)
 
+sns.lineplot(x="N", y="TIME", data=data)
+
+plt.title("Test Plot")
+plt.grid()
+plt.gca().invert_yaxis()
+plt.savefig("image.png")
