0f303feec74b9beef7ec0d60093fa3b30f258992
Mattia <mattiapizzolitto141@gmail.com>
2021-02-09 16:49:34 +0100

    Added function for clock resolution and for writing a dataset to a csv format

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <vector>
 #include "core.hpp"
 
 int main(int argc, char **argv)
 {
-    auto s = generate_random_str(100, 10);
+    auto s = generate_random_str(50, 10);
 
     std::cout << s << '\n';
+
+    auto time = get_system_resolution();
+
+    std::cout << time << "ns\n";
+
+    auto a = period_naive(s);
+    auto b = period_smart(s);
+
+    std::cout << "Naive implementation result: " << a << "\nSmart implementation result: " << b << '\n';
+
+    std::vector<std::pair<int, double>> v{{1, 1.00}, {5, 9.99}, {10, 10.00}};
+
+    write_csv("test.txt", v, "N", "TIME");
 
     return EXIT_SUCCESS;
 }
