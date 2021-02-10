33bcd0ffed5d5aa7aff8bb1be9bd918f1426bd5c
riccrusso2 <riccardo.russo@liceobanzi.edu.it>
2021-02-10 17:21:37 +0100

    funzioni plot

--- src/plotResults.py
+++ src/plotResults.py
@@ -1,21 +1,106 @@
 import csv
 import seaborn as sns
 import pandas as pd
+import math
+import matplotlib.pyplot as plt
 
-# apro il file csv e creo una lista con il nome delle colonne e una con i risultati
-with open("/content/tempi.csv", "r", encoding='utf-8-sig') as f:
-    lettore = csv.reader(f, delimiter=";")
-    col_names = next(lettore)
-    results = []
-    for line in lettore:
-        results.append(line)
 
-# creo un dataframe
-data = pd.DataFrame()
+def plotta_risultati(percorsoSmart, percorsoNaive):
+    # apro il file csv e creo una lista con il nome delle colonne e una con i risultati
 
-data[col_names[0]] = [item[0] for item in results]
+    with open(percorsoSmart, "r", encoding='utf-8-sig') as f:
+        lettore = csv.reader(f, delimiter=",")
+        col_names = next(lettore)
+        results = []
+        for line in lettore:
+            results.append(line)
 
-data[col_names[1]] = [item[1] for item in results]
+    # creo un dataframe
+    dataSmart = pd.DataFrame()
 
-sns.scatterplot(data.diversi_n, data.tempi)
+    dataSmart[col_names[0]] = [item[0] for item in results]
+
+    dataSmart[col_names[1]] = [item[1] for item in results]
+
+    dataSmart["TIME"] = pd.to_numeric(dataSmart["TIME"])
+
+    dataSmart["N"] = pd.to_numeric(dataSmart["N"])
+
+    # apro il file csv e creo una lista con il nome delle colonne e una con i risultati
+    with open(percorsoNaive, "r", encoding='utf-8-sig') as f:
+        lettore = csv.reader(f, delimiter=",")
+        col_names = next(lettore)
+        results = []
+        for line in lettore:
+            results.append(line)
+
+    # creo  un dataframe
+    data = pd.DataFrame()
+
+    data[col_names[0]] = [item[0] for item in results]
+
+    data[col_names[1]] = [item[1] for item in results]
+
+    data["TIME"] = pd.to_numeric(data["TIME"])
+
+    data["N"] = pd.to_numeric(data["N"])
+
+    sns.lineplot(data.N, data.TIME)
+    sns.lineplot(dataSmart.N, dataSmart.TIME)
+    sns.scatterplot(data.N, data.TIME)
+    sns.scatterplot(dataSmart.N, dataSmart.TIME)
+    plt.title("Stima tempi")
+    plt.show()
+
+
+
+
+
+def plotta_risultati(percorsoSmart, percorsoNaive):
+    # apro il file csv e creo una lista con il nome delle colonne e una con i risultati
+
+    with open(percorsoSmart, "r", encoding='utf-8-sig') as f:
+        lettore = csv.reader(f, delimiter=",")
+        col_names = next(lettore)
+        results = []
+        for line in lettore:
+            results.append(line)
+
+    # creo un dataframe
+    dataSmart = pd.DataFrame()
+
+    dataSmart[col_names[0]] = [item[0] for item in results]
+
+    dataSmart[col_names[1]] = [item[1] for item in results]
+
+    dataSmart["TIME"] = pd.to_numeric(dataSmart["TIME"])
+
+    dataSmart["N"] = pd.to_numeric(dataSmart["N"])
+
+    # apro il file csv e creo una lista con il nome delle colonne e una con i risultati
+    with open(percorsoNaive, "r", encoding='utf-8-sig') as f:
+        lettore = csv.reader(f, delimiter=",")
+        col_names = next(lettore)
+        results = []
+        for line in lettore:
+            results.append(line)
+
+    # creo  un dataframe
+    data = pd.DataFrame()
+
+    data[col_names[0]] = [item[0] for item in results]
+
+    data[col_names[1]] = [item[1] for item in results]
+
+    data["TIME"] = pd.to_numeric(data["TIME"])
+
+    data["N"] = pd.to_numeric(data["N"])
+
+    sns.lineplot(data.N, data.TIME)
+    sns.lineplot(dataSmart.N, dataSmart.TIME)
+    sns.scatterplot(data.N, data.TIME)
+    sns.scatterplot(dataSmart.N, dataSmart.TIME)
+    plt.title("Stima tempi")
+    plt.show()
+
 
